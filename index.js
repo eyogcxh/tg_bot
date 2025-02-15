5eebf1c2697e61061aa8cefde6204c5b0d9529ce
eygcxh <elogvinov19@gmail.com>
2025-02-15 14:59:42 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const TgApi = require('node-telegram-bot-api')
+const { options, again } = require('./options')
+
+const token = '7690931384:AAGifnBInN4x4FoP6BK8npPAGoaZA80RZ2E'
+
+const bot = new TgApi(token, { polling: true });
+
+const chats = {};
+
+const startGame = async (chatId) => {
+  await bot.sendMessage(chatId, 'Угадай цифру от 1 до 3');
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  chats[chatId] = `${randomNumber}`;
+  await bot.sendMessage(chatId, 'game test', options);
+};
+
+const start = () => {
+  bot.setMyCommands([
+    { command: '/start', description: 'Приветствие' },
+    { command: '/info', description: 'Информация о боте' },
+    { command: '/start_test', description: 'Игровой режим' }
+  ]);
+
+  bot.on('message', async msg => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    if (text === '/start') {
+      return bot.sendMessage(chatId, `Hello ${msg.from.first_name}`);
+    }
+    if (text === '/info') {
+      return bot.sendMessage(chatId, 'info text');
+    }
+    if (text === '/start_test') {
+      return startGame(chatId);
+    }
+
+    return bot.sendMessage(chatId, 'Я тебя не понимаю');
+  });
+
+  bot.on('callback_query', async query => {
+    const data = query.data;
+    const chatId = query.message.chat.id;
+
+    await bot.answerCallbackQuery(query.id);
+
+    if (data === 'again') {
+      return startGame(chatId);
+    }
+
+    if (data === chats[chatId]) {
+      return bot.sendMessage(chatId, `Молодец, ты отгадал цифру ${chats[chatId]}! 🎉`, again);
+    } else {
+      return bot.sendMessage(chatId, `Ты не угадал 😔 Загадана цифра ${chats[chatId]}.`, again);
+    }
+  });
+};
+
+start();
---
+++ options.js
@@ -0,0 +1,19 @@
+module.exports = {
+  options: {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: '1', callback_data: '1' }],
+        [{ text: '2', callback_data: '2' }],
+        [{ text: '3', callback_data: '3' }]
+      ]
+    }
+  },
+  
+  again: {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Начать заново', callback_data: 'again' }]
+      ]
+    }
+  }
+}
